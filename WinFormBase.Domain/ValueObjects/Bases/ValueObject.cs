0f0039bc17ee966c1b68dceb70363bd36016fbfc
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2024-12-08 18:30:23 +0900

    ValueObjectの作成

---
+++ WinFormBase.Domain/ValueObjects/Bases/ValueObject.cs
@@ -0,0 +1,31 @@
+﻿namespace WinFormBase.Domain.ValueObjects.Bases;
+
+public abstract class ValueObject<T> where T : ValueObject<T>
+{
+    public override bool Equals(object? obj)
+    {
+        if (obj is not T value)
+        {
+            return false;
+        }
+
+        return this.EqualsCore(value);
+    }
+
+    public static bool operator ==(ValueObject<T> vo1, ValueObject<T> vo2)
+    {
+        return Equals(vo1, vo2);
+    }
+
+    public static bool operator !=(ValueObject<T> vo1, ValueObject<T> vo2)
+    {
+        return !Equals(vo1, vo2);
+    }
+
+    protected abstract bool EqualsCore(T? other);
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
