86fb9747d1e5cb4c36542d23b86e6bb590e90173
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2024-12-08 18:05:43 +0900

    Exceptionsフォルダとベースクラスの作成

---
+++ WinFormBase.Domain/Exceptions/InputException.cs
@@ -0,0 +1,14 @@
+﻿using WinFormBase.Domain.Exceptions.Bases;
+
+namespace WinFormBase.Domain.Exceptions;
+
+public sealed class InputException : ExceptionBase
+{
+    public InputException() : base() { }
+
+    public InputException(string message) : base(message) { }
+
+    public InputException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    public override ExceptionType ExceptionType => ExceptionType.Error;
+}
