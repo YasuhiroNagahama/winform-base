b4f285a7602912e4b763a85858c2473ce0113093
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2024-12-08 14:10:25 +0900

    ViewModelBaseの実装

--- WinFormBase/ViewModels/Bases/FormViewModelBase.cs
+++ WinFormBase/ViewModels/Bases/FormViewModelBase.cs
@@ -1,5 +1,46 @@
 ﻿namespace WinFormBase.ViewModels.Bases;
 
-public sealed class FormViewModelBase
+public abstract class FormViewModelBase : ViewModelBase
 {
+    private int _statusProgressValue = 0;
+    public int StatusProgressValue
+    {
+        get => this._statusProgressValue;
+        set => this.SetProperty(ref this._statusProgressValue, value);
+    }
+
+    private int _statusProgressMaximum = 0;
+    public int StatusProgressMaximum
+    {
+        get => this._statusProgressMaximum;
+        set => this.SetProperty(ref this._statusProgressMaximum, value);
+    }
+
+    private int _statusProgressMinimum = 0;
+    public int StatusProgressMinimum
+    {
+        get => this._statusProgressMinimum;
+        set => this.SetProperty(ref this._statusProgressMinimum, value);
+    }
+
+    private bool _statusProgressVisible;
+    public bool StatusProgressVisible
+    {
+        get => this._statusProgressVisible;
+        set => this.SetProperty(ref this._statusProgressVisible, value);
+    }
+
+    private ProgressBarStyle _statusProgressStyle;
+    public ProgressBarStyle StatusProgressStyle
+    {
+        get => this._statusProgressStyle;
+        set => this.SetProperty(ref this._statusProgressStyle, value);
+    }
+
+    private string _statusLabelText = String.Empty;
+    public string StatusLableText
+    {
+        get => this._statusLabelText;
+        set => this.SetProperty(ref this._statusLabelText, value);
+    }
 }
---
+++ WinFormBase/ViewModels/Bases/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WinFormBase.ViewModels.Bases;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected bool SetProperty<T>(ref T field,
+        T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (Equals(field, value))
+        {
+            return false;
+        }
+
+        field = value;
+
+        if (PropertyChanged is not null && propertyName is not null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        return true;
+    }
+}
