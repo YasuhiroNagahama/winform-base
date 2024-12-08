08d20b8c7ae6b80e9b744839966288903c350983
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2024-12-08 16:50:14 +0900

    UtilityHelperクラスを作成

---
+++ WinFormBase/Utilities/UtilityHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace WinFormBase.Utilities;
+
+public static class UtilityHelper
+{
+    public static T? TryGetValue<T>(IList<T> values, int i)
+    {
+        return i >= 0 && values.Count > i ? values[i] : default;
+    }
+}
