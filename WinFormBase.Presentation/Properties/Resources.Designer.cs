3f52617f6cdeb0f0bb0f5bc84c042584954b4bdc
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2025-02-01 09:21:58 +0900

    style:プロジェクト名の変更

--- WinFormBase.Presentation/Properties/Resources.Designer.cs
+++ WinFormBase.Presentation/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormBase.WinForm.Properties {
+namespace Base.WinForm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormBase.WinForm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormBase.WinForm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Base.WinForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
