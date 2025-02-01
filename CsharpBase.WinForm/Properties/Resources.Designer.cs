d48de99e715a8801a012f9a313c7e0f59b6cfd89
Takemura Yasuhiro <fnifhubi85h29bddi@gmail.com>
2025-02-01 10:05:22 +0900

    refactor: ソリューション、プロジェクトの変更

--- CsharpBase.WinForm/Properties/Resources.Designer.cs
+++ CsharpBase.WinForm/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Base.WinForm.Properties {
+namespace CsharpBase.WinForm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Base.WinForm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Base.WinForm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CsharpBase.WinForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
